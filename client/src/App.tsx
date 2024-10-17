3ac2df2c5e743fe00fb0e6c2d1a9026b6dee48a8
Ektoplasme <kainomel@hotmail.fr>
2024-10-17 17:45:21 +0200

    cfg: Adds JoyUI to the client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import Logo from "/Logo.svg";
 import "./App.css";
+import Button from "@mui/joy/Button";
 
 function App() {
   return (
@@ -7,6 +8,14 @@ function App() {
       <img src={Logo} className="logo" alt="Perfect Match logo" />
       <h1>Hello, ici le client de Purrfect Match!</h1>
       <p>Il y a encore tout à construire, bon courage 🐱</p>
+      <Button
+        color="primary"
+        size="lg"
+        onClick={() => alert("JoyUI Fonctionne!")}
+        variant="solid"
+      >
+        Bouton pour tester JoyUI
+      </Button>
     </>
   );
 }
