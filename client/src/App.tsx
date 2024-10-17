89821bc288d7219467da0c050de9705638080ebf
Ektoplasme <kainomel@hotmail.fr>
2024-10-17 17:56:57 +0200

    cfg: Fixes logo animation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,19 +4,23 @@ import Button from "@mui/joy/Button";
 
 function App() {
   return (
-    <>
-      <img src={Logo} className="logo" alt="Perfect Match logo" />
-      <h1>Hello, ici le client de Purrfect Match!</h1>
-      <p>Il y a encore tout à construire, bon courage 🐱</p>
-      <Button
-        color="primary"
-        size="lg"
-        onClick={() => alert("JoyUI Fonctionne!")}
-        variant="solid"
-      >
-        Bouton pour tester JoyUI
-      </Button>
-    </>
+    <div className="container">
+      <div className="logo-container">
+        <img src={Logo} className="logo" alt="Perfect Match logo" />
+      </div>
+      <div className="content">
+        <h1>Hello, ici le client de Purrfect Match!</h1>
+        <p>Il y a encore tout à construire, bon courage 🐱</p>
+        <Button
+          color="primary"
+          size="lg"
+          onClick={() => alert("JoyUI Fonctionne!")}
+          variant="solid"
+        >
+          Bouton pour tester JoyUI
+        </Button>
+      </div>
+    </div>
   );
 }
 
