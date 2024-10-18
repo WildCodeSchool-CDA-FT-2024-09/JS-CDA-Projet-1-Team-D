df77ded29e8a47c3a02c7994b100279ccf364e27
Ektoplasme <kainomel@hotmail.fr>
2024-10-18 17:01:58 +0200

    cfg: Adds apollo client config in frontend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./views/Home";
 import { NoPage } from "./views/404";
 import "./index.css";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
 export default function App() {
   return (
     <StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
-      </BrowserRouter>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NoPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ApolloProvider>
     </StrictMode>
   );
 }
