653be157a7c7316bbd72a83281baee7eb6bfd27b
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-10-31 14:25:53 +0100

    feat: login mutation resolver

--- server/src/cats/cat.resolvers.ts
+++ server/src/cats/cat.resolvers.ts
@@ -1,10 +1,33 @@
 import { Cat } from "../cats/cat.entities";
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { LogginInfosInput } from "./cat.types";
+import argon2 from "argon2";
 
 @Resolver(Cat)
 export default class CatResolver {
   @Query(() => [Cat])
   async getCatResolvers() {
     return [];
+  }
+
+  @Mutation(() => Cat)
+  async login(@Arg("data") logginInfos: LogginInfosInput) {
+    const { email, password } = logginInfos;
+
+    const cat = await Cat.findOne({
+      where: { email },
+    });
+
+    if (!cat) {
+      throw new Error("L'utilisateur n'est pas enregistré.");
+    }
+
+    const passwordValidation = await argon2.verify(cat.password, password);
+
+    if (!passwordValidation) {
+      throw new Error("Le mot de passe est incorrect.");
+    }
+
+    return cat;
   }
 }
