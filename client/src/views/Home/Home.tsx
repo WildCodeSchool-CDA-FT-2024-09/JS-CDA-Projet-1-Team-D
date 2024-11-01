c74c79bff4c309acd6a7ea0934a5f1dd54a310da
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-11-01 15:23:12 +0100

    fix: replaced joy UI Button to Link component & added CSS to style it

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,11 +1,8 @@
 import Logo from "/Logo.svg";
 import "./Home.css";
-import Button from "@mui/joy/Button";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-  const navigate = useNavigate();
-
   return (
     <section className="home-container">
       <section className="logo-container">
@@ -13,15 +10,9 @@ export const Home = () => {
       </section>
       <h1>Hello, ici le client de Purrfect Match!</h1>
       <p>Il y a encore tout à construire, bon courage 🐱</p>
-      <Button
-        size="lg"
-        color="primary"
-        onClick={() => navigate("/login")}
-        variant="solid"
-        className="connexion-button"
-      >
+      <Link className="redirect-co-page" to="/login">
         Connexion
-      </Button>
+      </Link>
     </section>
   );
 };
