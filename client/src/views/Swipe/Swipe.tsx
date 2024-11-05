e805d4943cc41cc50092a91eb392a39977bef9b3
Ektoplasme <kainomel@hotmail.fr>
2024-11-05 16:00:36 +0100

    feat: Adds pagination system and comments
    feat: Adds cards refetch on component mount

--- client/src/views/Swipe/Swipe.tsx
+++ client/src/views/Swipe/Swipe.tsx
@@ -14,26 +14,69 @@ import { useSpring, useSprings, animated } from "@react-spring/web";
 import { useDrag } from "@use-gesture/react";
 import { CSSProperties, useEffect, useState } from "react";
 import {
+  Cat,
+  // useLikedCatsQuery,
   useSendDislikeMutation,
   useSendLikeMutation,
   useSwipeListQuery,
 } from "../../generated/graphql-types";
 
-const stackLength = 20;
+const stackLength = 5;
+const connectedCatId = 25;
 
 export const Swipe = () => {
-  const connectedCatId = 25;
+  // Cursor pour savoir où on en est dans la liste entière des chats
+  const [catsListCursor, setCatsListCursor] = useState(stackLength);
 
-  const { data, loading, error } = useSwipeListQuery({
-    variables: {
-      catId: connectedCatId,
-    },
-  });
+  // Partie de la liste entière des chats (egale à stacklength), pour ne pas tous les afficher d'un coup
+  const [currentCatsList, setCurrentCatsList] = useState<Cat[]>([]);
 
+  // Index de la carte (entre 0 et stacklength) sur laquelle on est
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+
+  // State pour les like et dislike, pour le style uniquement
+  const [isLike, setIsLike] = useState(false);
+  const [isDislike, setIsDislike] = useState(false);
+
+  // Mutations et queries
   const [sendLikeMutation] = useSendLikeMutation();
   const [sendDislikeMutation] = useSendDislikeMutation();
 
-  const dataCats = data?.swipeList?.slice(0, stackLength);
+  const { data, loading, error, refetch } = useSwipeListQuery({
+    variables: {
+      catId: connectedCatId,
+    },
+    fetchPolicy: "cache-and-network",
+  });
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  // Remplissage de la liste de chats à afficher,
+  // se met à jour à chaque fois que le cursor change ou data se remplit
+  useEffect(() => {
+    const updatedCatsList: Cat[] = data?.swipeList?.slice(
+      catsListCursor - stackLength,
+      catsListCursor
+    );
+    if (updatedCatsList) setCurrentCatsList(updatedCatsList);
+  }, [data, catsListCursor]);
+
+  // Fonction appellée à chaque fois qu'il n'y a plus de carte à swipe
+  // pour augmenter le curseur et donc trigger le useEffect qui remplira la liste des chats à afficher
+  // avec les profils suivants
+  const nextPagination = () => {
+    // On rajoute 'stacklength' au cursor car c'est le nombre de carte
+    // qu'on veut afficher à chaque fois (à determiner)
+    setCatsListCursor((prev) => prev + stackLength);
+
+    // On remet l'index de la carte courante à zero, car on a redistribué des cartes
+    setCurrentCardIndex(0);
+  };
+
+  // Tout ici est relatif à Springs, et donc aux animations des cartes
+  // et des icones like et dislike
 
   const from = () => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
   const to = (i: number) => ({
@@ -44,10 +87,14 @@ export const Swipe = () => {
     delay: i * 100,
   });
 
-  const [props, springApi] = useSprings(stackLength, (i) => ({
-    ...to(i),
-    from: from(),
-  }));
+  const [props, springApi] = useSprings(
+    stackLength,
+    (i) => ({
+      ...to(i),
+      from: from(),
+    }),
+    [currentCatsList]
+  );
 
   const [{ likeVsblt, iconScale, trans, op }, likeIconApi] = useSpring(() => ({
     likeVsblt: "hidden",
@@ -60,11 +107,6 @@ export const Swipe = () => {
     dislikeVsblt: "hidden",
   }));
 
-  const [isLike, setIsLike] = useState(false);
-  const [isDislike, setIsDislike] = useState(false);
-
-  const [currentCardIndex, setCurrentCardIndex] = useState(0);
-
   useEffect(() => {
     likeIconApi.start(() => {
       return { likeVsblt: isLike ? "visible" : "hidden", trans: "0.1s" };
@@ -74,8 +116,44 @@ export const Swipe = () => {
     });
   }, [isLike, isDislike]);
 
+  // fonction qui appelle la mutation sendLike
+  const likeCat = async (catIndex: number) => {
+    if (!currentCatsList || !currentCatsList[catIndex]) return;
+    const catToLikeId = currentCatsList[catIndex].id;
+
+    try {
+      await sendLikeMutation({
+        variables: {
+          catId1: connectedCatId,
+          catId2: catToLikeId,
+        },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // fonction qui appelle la mutation sendDislike
+  const dislikeCat = async (catIndex: number) => {
+    if (!currentCatsList || !currentCatsList[catIndex]) return;
+    const catToDislikeId = currentCatsList[catIndex].id;
+
+    try {
+      await sendDislikeMutation({
+        variables: {
+          catId1: connectedCatId,
+          catId2: catToDislikeId,
+        },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // Fonction qui se declenche au clique du bouton like
   const handleLikeClick = () => {
     if (currentCardIndex >= stackLength) {
+      nextPagination();
       return;
     }
     setIsLike(true);
@@ -90,8 +168,10 @@ export const Swipe = () => {
     setTimeout(() => setIsLike(false), 300);
   };
 
+  // Fonction qui se declenche au clique du bouton dislike
   const handleDislikeClick = () => {
     if (currentCardIndex >= stackLength) {
+      nextPagination();
       return;
     }
     setIsDislike(true);
@@ -106,51 +186,39 @@ export const Swipe = () => {
     setTimeout(() => setIsDislike(false), 300);
   };
 
-  const likeCat = async (catIndex: number) => {
-    if (!dataCats || !dataCats[catIndex]) return;
-    const catToLikeId = dataCats[catIndex].id;
-
-    try {
-      await sendLikeMutation({
-        variables: {
-          catId1: connectedCatId,
-          catId2: catToLikeId,
-        },
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const dislikeCat = async (catIndex: number) => {
-    if (!dataCats || !dataCats[catIndex]) return;
-    const catToDislikeId = dataCats[catIndex].id;
-
-    try {
-      await sendDislikeMutation({
-        variables: {
-          catId1: connectedCatId,
-          catId2: catToDislikeId,
-        },
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
+  // Hook useDrag de la librairie react-use-gesture nous permettant d'utiliser
+  // l'évenement 'drag' (glisser) sur des divs selectionnées
+  // On assigne cette fonction 'bind' aux divs pour lesquelles on aimerait utiliser
+  // l'evenement drag et toutes les conditions qu'on y ajoute
   const bind = useDrag(({ args: [index], down, movement: [mx, my] }) => {
+    // Ici on va mapper parmi tous les "springs"
+    // aka toutes les animations (ici une par carte)
     springApi.start((i) => {
+      // Si on est pas sur la carte courante on ne fait rien
       if (index !== i) return;
+
+      // Si la carte (à l'évenement drag, glisser) se situe
+      // à 150px du centre de la page ou superieur
       if (mx >= 150) {
+        // On passe le state de like à true
         if (!isLike) setIsLike(true);
 
+        // Si on a relaché le clic (ça veut dire on a liké)
         if (!down) {
+          // On like le cat
           likeCat(index);
+          // On passe à l'index du cat suivant
           setCurrentCardIndex(index + 1);
         }
+        // On envoie la carte courante voler à droite bien loin
+        // Comme ça on la voit plus
         return { x: down ? mx : 1000, y: down ? my : 0 };
       }
+
+      // Si la carte (à l'évenement drag, glisser) se situe
+      // à -150px du centre de la page ou inferieur
       if (mx <= -150) {
+        // Meme logique mais inverse que pour le like au dessus
         if (!isDislike) setIsDislike(true);
 
         if (!down) {
@@ -159,10 +227,19 @@ export const Swipe = () => {
         }
         return { x: down ? mx : -1000, y: down ? my : 0 };
       }
+
+      // Si la carte se trouve au centre entre les
+      // deux breakpoint
       if ((mx > -150 && mx < 180) || !down) {
+        // On annule les states de like/dislike
+        // (pour retirer le coeur ou la croix)
         if (isLike) setIsLike(false);
         if (isDislike) setIsDislike(false);
       }
+
+      // Dans tous les autres cas si la souris est en clic (down)
+      // on retourne la position actuelle: la carte restera sous la souris donc
+      // SINON (si on a laché le clic) on la remet au centre de l'écran
       return {
         x: down ? mx : 0,
         y: down ? my : 0,
@@ -170,13 +247,23 @@ export const Swipe = () => {
         scale: down ? 1.1 : 1,
       };
     });
+
+    // Ici on est en dehors du spring mais toujours dans le hook drag
+    // Pour detecter si on lache le clic de souris
     if (!down) {
+      // si c'est le cas, comme au dessus on annule les states de like/dislike
+      // (pour retirer le coeur ou la croix) mais apres un petit temps pour
+      // montrer à l'user le like ou non like qu'il a fait
       setTimeout(() => {
         if (isLike) setIsLike(false);
         if (isDislike) setIsDislike(false);
       }, 300);
     }
+
+    // Ici on est sur le spring uniquement relié aux icones like et dislike
     likeIconApi.start(() => {
+      // On joue avec l'opacité des icones en fonction de si on est
+      // dans la zone de breakpoint ou pas
       if (mx >= 150) {
         return { op: `${50 + mx / 5}%`, iconScale: `${0.5 + mx / 4}%` };
       }
@@ -265,10 +352,10 @@ export const Swipe = () => {
                 touchAction: "none",
               }}
             >
-              {dataCats && dataCats[i] && (
+              {currentCatsList && currentCatsList[i] && (
                 <SwipeCard
-                  {...dataCats[i]}
-                  age={getCatAge(dataCats[i].birthday)}
+                  {...currentCatsList[i]}
+                  age={getCatAge(currentCatsList[i].birthday)}
                   interests={mockedInterests}
                 />
               )}
