7dcaef8a10339b3a0aa82565b97d5ceda107c586
Pas-nou <pascal.renou@live.fr>
2024-11-06 14:40:09 +0100

    feat: add button on front profile card for delete like

--- client/src/components/ProfileCard/ProfileLikeCard.tsx
+++ client/src/components/ProfileCard/ProfileLikeCard.tsx
@@ -11,20 +11,29 @@ export const ProfileLikeCard = ({
   id,
 }: LikedCat) => {
   return (
-    <Link to={`/profile/${id}`} className="profile-card-link">
-      <article className="profile-card-container">
-        <img
-          className="profile-picture-avatar"
-          src={profile_picture}
-          alt={`photo de profil de ${name}`}
-        />
-        <div className="profile-card-text">
-          <p className="profile-surname">{surname}</p>
-          <h3 className="profile-texts">
-            {name}, {calculateAge(birthday)}
-          </h3>
-        </div>
-      </article>
-    </Link>
+    <div className="profile-card-global">
+      <button
+        type="button"
+        className="profile-like-card-button"
+        onClick={() => alert("Coucou")}
+      >
+        <img src="/croix-delete.png" alt="bouton pour supprimer un like" />
+      </button>
+      <Link to={`/profile/${id}`} className="profile-card-link">
+        <article className="profile-card-container">
+          <img
+            className="profile-picture-avatar"
+            src={profile_picture}
+            alt={`photo de profil de ${name}`}
+          />
+          <div className="profile-card-text">
+            <p className="profile-surname">{surname}</p>
+            <h3 className="profile-texts">
+              {name}, {calculateAge(birthday)}
+            </h3>
+          </div>
+        </article>
+      </Link>
+    </div>
   );
 };
